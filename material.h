8640838ac7a510b56489ed1d8270caa695521566
bugisone <937459807@qq.com>
2019-10-08 21:07:56 +0800

    左边glass显示不正常全反射光

--- material.h
+++ material.h
@@ -1,6 +1,7 @@
 #ifndef MATERIALH
 #define MATERIALH
 #include "hitable.h"
+#include "drand48.h"
 
 // pick a random point "in" the cube. z axis between -1 to +1
 vec3 random_in_unit_sphere() {
@@ -28,6 +29,12 @@ bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted){
     return false;
 }
 
+// for glass, a simple polynomial approximation by Schlick
+float schlick(float cosine, float ref_idx){
+  float r0 = (1-ref_idx) / (1+ref_idx);
+  r0 = r0*r0;
+  return r0 + (1-r0)*pow((1 - cosine), 5);
+}
 
 class material{
   public:
@@ -50,12 +57,13 @@ class lambertian: public material {
 
 class metal: public material {
   public:
-    metal(const vec3& a, float f): albedo(a) {
-      if(f < 1)
-        fuzz = f;
-      else
-        fuzz = 1;
-    }
+    metal(const vec3& a): albedo(a) {}
+    // metal(const vec3& a, float f): albedo(a) {
+    //   if(f < 1)
+    //     fuzz = f;
+    //   else
+    //     fuzz = 1;
+    // }
     virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuaion, ray& scattered) const{
       vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
       // the bigger the sphere, the fuzzier the reflections will be
@@ -73,30 +81,40 @@ class dielectric: public material {
     dielectric(float ri): ref_idx(ri) {}
     virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const{
       vec3 outward_normal;
-      // for glass, there is no reflected, but refraction
+      // for dielectric, there is reflected, and refraction
       vec3 reflected = reflect(r_in.direction(), rec.normal);
       float ni_over_nt;
       // kill the blue channel (?whaaat)
       attenuation = vec3(1.0, 1.0, 1.0);
       vec3 refracted;
+      float reflect_prob;
+      float cosine;
+
       if(dot(r_in.direction(), rec.normal) > 0)
       {
         outward_normal = -rec.normal;
         ni_over_nt = ref_idx;
+        cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
       }
       else
       {
         outward_normal = rec.normal;
         ni_over_nt = 1.0 / ref_idx;
+        cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
       }
       
       if(refract(r_in.direction(), outward_normal, ni_over_nt, refracted))
-        scattered = ray(rec.p, refracted);
+        reflect_prob = schlick(cosine, ref_idx);
       else
       {
         scattered = ray(rec.p, refracted);
-        return false;
+        reflect_prob = 1.0;
       }
+
+      if(drand48() < reflect_prob)
+        scattered = ray(rec.p, reflected);
+      else
+        scattered = ray(rec.p, reflected);
 
       return true;
     }
